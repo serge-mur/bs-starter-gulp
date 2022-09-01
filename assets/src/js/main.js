c3fc99702b6c1c7d5b50c840077fa6f31a84bc97
serge <serge-mur@yandex.ru>
2022-09-01 11:28:35 +0300

    first commit

---
+++ assets/src/js/main.js
@@ -0,0 +1,8 @@
+// Импортируем jQuery
+//=include('../../../node_modules/jquery/dist/jquery.js')
+
+// Импортируем Bootstrap 5
+//=include('../../../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js')
+
+// Импортируем другие js-файлы
+//=include('custom.js')
